e482d4221a9447ef0370a6976941afd7a2e9b37d
Pritesh Soni <priteshsoni263@gmail.com>
2022-02-11 17:57:55 +0200

    project button color changed

--- src/components/Projects/ProjectsStyles.js
+++ src/components/Projects/ProjectsStyles.js
@@ -84,14 +84,19 @@ export const UtilityList = styled.ul`
 `;
 
 export const ExternalLinks = styled.a`
-  color: #d4c0c0;
+  color: #000;
+
   font-size: 1.6rem;
   padding: 1rem 1.5rem;
-  background: #6b3030;
+  /* background: #6b3030; */
+  background: ${({ theme }) => theme.lightTheme.mainColor};
+  opacity: 0.7;
   border-radius: 15px;
   transition: 0.5s;
   &:hover {
-    background: #801414;
+    /* background: #801414; */
+    background: "#ffffff";
+    opacity: 1;
   }
 `;
 
